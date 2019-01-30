39e7a57a97065712c99f3207d1970975d7cc3024
Nateanidus <nateanidus@gmail.com>
2019-01-30 16:06:00 -0700

    comment changed

--- ArrayObject.java
+++ ArrayObject.java
@@ -2,7 +2,7 @@ public class ArrayObject{
     public static void main(String[] args) {
         int [] myIntArray = {1, 3, 4, 5,};// unused array
         Book[] bookArray = new Book[7];
-        bookArray[0] = new Book("Daniel", "As Daniel Codes","2019");
+        bookArray[0] = new Book("Daniel", "As Daniel Codes","2019");//print shit
         System.out.println("The title of the book is: " + bookArray[0].title);
         
         bookArray[0].title = "A new title";
